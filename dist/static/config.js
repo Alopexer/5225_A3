d663634b55208ced587b226d47ef2eebb6e60708
alan <zouu0007@student.monash.edu>
2025-06-10 03:06:16 +1000

    ui upload

---
+++ dist/static/config.js
@@ -0,0 +1,10 @@
+const config = {
+    clientId: '6clo2o14jnc5t3drktfg01bs26',
+    logoutUri: 'localhost:https://staging.d3ekpadjpc4bz2.amplifyapp.com',
+    cognitoDomain: 'https://us-east-1brux9wqxr.auth.us-east-1.amazoncognito.com',
+    authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_BRux9wqxr',
+    redirect_uri: 'https://staging.d3ekpadjpc4bz2.amplifyapp.com',
+    response_type: 'code',
+    scope: 'openid email phone'
+
+}
